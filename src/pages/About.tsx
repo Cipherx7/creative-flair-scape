645c8511f53bbb2612487939d0bfe3350f3b4df8
Abhishek pawar <app.stu2003@gmail.com>
2025-03-14 11:54:11 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -32,7 +32,7 @@ const About = () => {
             <ScrollReveal delay={200}>
               <div>
                 <img
-                  src="https://images.unsplash.com/photo-1556761175-b413da4ca6d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                  src="https://i.ibb.co/3yybBDjY/final-logo-divine-da.png"
                   alt="Elegant wedding setup"
                   className="rounded-lg shadow-md"
                 />
